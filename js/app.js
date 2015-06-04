b807d1e708d86c5b05828524f3d8fc99a23f6dc7
mdpinar <mdumlupinar@gmail.com>
2015-06-04 17:43:20 +0300

    faq sayfasi eklendi

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ seyhanSiteApp.config(function($routeProvider) {
     $routeProvider
         .when('/', {templateUrl : 'main.html'})
         .when('/license', {templateUrl : 'license.html'})
+        .when('/faq', {templateUrl : 'faq.html'})
         .when('/contact', {templateUrl : 'contact.html'})
 
         .otherwise({ redirectTo: '/' });
